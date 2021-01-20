3ca5e21c08ce895b17074d3070f319585a030729
barbosayyy <pedro_barbosa2000@hotmail.com>
2021-01-20 20:16:30 +0000

    Added delay on click
    Jquery library to all scripts and new delay on <a> clicking.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var audio = new Audio();
+var timer = document.getElementById('jstimer')
+
 if (document.title != "Tongue-Cut Sparrow Index")
 {
 	audio.src = "../audio/button.mp3";
@@ -8,4 +10,12 @@ else
 	audio.src = "audio/button.mp3";
 }
 
-
+$(function(){
+    $("a").click(function(evt){
+			evt.preventDefault();
+        var link = $(this).attr("href");
+        setTimeout(function() {
+            window.location.href = link;
+        }, 500);
+    });
+});
